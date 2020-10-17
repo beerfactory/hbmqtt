979553c24fb311d13457319c4d87d18475e7537e
Ville Skyttä <ville.skytta@iki.fi>
2020-10-17 22:54:28 +0300

    Exclude tests subpackages from installs too

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email='nico@beerfactory.org',
     url="https://github.com/beerfactory/hbmqtt",
     license='MIT',
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     platforms='all',
     install_requires=[
