42e60d6a0970ead32ab9a5db3b9b461354cc1031
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2021-01-09 21:14:46 +0100

    Remove python3.4 from tox. Deprecated

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Communications',
